58afa8ed2c8f9730e1968a3b5077375948d9256e
1234cabs <120103959+1234cabs@users.noreply.github.com>
2023-06-14 19:20:00 +0800

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,39 @@
+let menu = document.querySelector('#menu-btn');
+let header = document.querySelector('.header');
+
+menu.onclick = () =>{
+    menu.classList.toggle('fa-times');
+    header.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    header.classList.remove('active');
+}
+
+let themeToggler = document.querySelector('#theme-toggler');
+
+themeToggler.onclick = () =>{
+    themeToggler.classList.toggle('fa-sun');
+    if(themeToggler.classList.contains('fa-sun')){
+        document.body.classList.add('active');
+    }else{
+        document.body.classList.remove('active');
+    }
+}
+/*===== SCROLL REVEAL ANIMATION =====*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+ScrollReveal().reveal(".content", {delay: 400})
+ScrollReveal().reveal(".IM", {delay: 300})
+ScrollReveal().reveal(".btn", {delay: 200})
+ScrollReveal().reveal(".share", {delay: 200})
+
+
+// /*SCROLL content*/
+// sr.reveal('.content',{delay: 400});
